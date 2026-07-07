628026ceff1566fd895b56499b2a91054a0e2e94
AkhilxRawat <akhilraw04@gmail.com>
2026-07-07 12:29:25 +0530

    Added a  demo pipeline

---
+++ sample.test.js
@@ -0,0 +1,9 @@
+describe('Sample Test Suite', () => {
+  test('should verify arithmetic operations', () => {
+    expect(1 + 1).toBe(2);
+  });
+
+  test('should verify string comparison', () => {
+    expect('Distill').toBe('Distill');
+  });
+});
